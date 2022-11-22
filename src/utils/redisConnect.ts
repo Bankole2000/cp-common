2d185d0735f739b0b80506dd3ec5d9414a1a150b
Bankole Esan <techybanky@gmail.com>
2022-11-22 23:54:23 +0100

    Updates

--- src/utils/redisConnect.ts
+++ src/utils/redisConnect.ts
@@ -1,5 +1,7 @@
-import { createClient, RedisClientType } from 'redis';
-import NRP, { NodeRedisPubSub } from 'node-redis-pubsub';
+// import { createClient, RedisClientType } from 'redis';
+import { createClient } from 'redis';
+// import NRP, { NodeRedisPubSub } from 'node-redis-pubsub';
+import NRP from 'node-redis-pubsub';
 
 export const redisConnect = (url: string, scope: string) => {
   const pubSub = NRP({ url, scope });
