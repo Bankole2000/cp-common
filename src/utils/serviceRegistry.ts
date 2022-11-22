9dfc78050b6877651a40c50fbfa47d79a4a847c0
Bankole Esan <techybanky@gmail.com>
2022-11-23 00:08:28 +0100

    Updates

--- src/utils/serviceRegistry.ts
+++ src/utils/serviceRegistry.ts
@@ -1,6 +1,7 @@
 import { RedisConnection } from './redisConnect';
 
 export const serviceUp = async (redis: RedisConnection, list: string, serviceData: any) => {
+  await redis.client.connect();
   const services = await redis.client?.get(list);
   const upServices = await JSON.parse(services || '{}');
 
@@ -11,12 +12,15 @@ export const serviceUp = async (redis: RedisConnection, list: string, serviceDat
     };
     await redis.client?.set(list, JSON.stringify(upServices));
     console.log(`${serviceData.self.serviceName} newly Registered`);
+    await redis.client.disconnect();
     return;
   }
   console.log(`${serviceData.self.serviceName} already Registered`);
+  await redis.client.disconnect();
 };
 
 export const serviceDown = async (redis: RedisConnection, list: string, serviceName: string) => {
+  await redis.client.connect();
   const services = await redis.client?.get(list);
   const upServices = await JSON.parse(services || '{}');
 
@@ -24,7 +28,9 @@ export const serviceDown = async (redis: RedisConnection, list: string, serviceN
     delete upServices[`${serviceName}`];
     await redis.client?.set(list, JSON.stringify(upServices));
     console.log(`${serviceName} De-registered`);
+    await redis.client.disconnect();
     return;
   }
   console.log(`${serviceName} already De-registered`);
+  await redis.client.disconnect();
 };
