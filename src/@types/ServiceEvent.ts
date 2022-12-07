83fbcc610b43a0c84ebb69c6e3dbdd1a27839e8a
Bankole Esan <techybanky@gmail.com>
2022-12-07 13:51:20 +0100

    Updates

--- src/@types/ServiceEvent.ts
+++ src/@types/ServiceEvent.ts
@@ -12,18 +12,22 @@ export class ServiceEvent implements AppEvent {
 
   accessToken: string | null;
 
+  serviceQueues: string[];
+
   constructor(
     type: string,
     data: any,
     idToken: string | null,
     accessToken: string | null,
     // origin = config.get<string>('self.serviceName')
-    origin = process.env.SERVICE_NAME || 'UNKNOWN'
+    origin = process.env.SERVICE_NAME || 'UNKNOWN',
+    serviceQueues = [],
   ) {
     this.type = type;
     this.data = data;
     this.origin = origin;
     this.idToken = idToken;
     this.accessToken = accessToken;
+    this.serviceQueues = serviceQueues;
   }
 }
